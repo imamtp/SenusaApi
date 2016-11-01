dd97602b284e97b9ff330f1a7b79fb962ab4d895
rachmatprabowo <praboworachmat@gmail.com>
2016-11-01 11:09:16 +0700

    add online and call counter

--- config/routes.js
+++ config/routes.js
@@ -68,6 +68,8 @@ module.exports.routes = {
   'get /kom': 'KomoditasController.find',
   'get /infoharga' : 'InfoHargaKKPController.find',
   'get /formImage':'Uploadimageapi.formImage',
-  'post /uploadImage':'Uploadimageapi.uploadImage'
+  'post /uploadImage':'Uploadimageapi.uploadImage',
+  'post /increaseonline': 'CounterController.increase',
+  'get /countertoday': 'CounterController.countertoday',
   // 'get /me': UserController.hi'allproduct
 };
