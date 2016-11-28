a09194cbd7debf01bc5bd00399d869208a94e56e
rachmatprabowo <praboworachmat@gmail.com>
2016-11-28 11:27:03 +0700

    add api log call

---
+++ api/models/LogCall.js
@@ -0,0 +1,19 @@
+/**
+ * LogCall.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+moment = require('moment')
+module.exports = {
+
+  attributes: {
+  	datetime: {type:'datetime', defaultsTo:moment().format('YYYY-MM-DD HH:mm:ss')},
+  	user:{model:'user'}
+  },
+  log: function(inputs, cb){
+  	LogCall.create({user:inputs.user_id})
+  	.exec(cb)
+  }
+};
+
